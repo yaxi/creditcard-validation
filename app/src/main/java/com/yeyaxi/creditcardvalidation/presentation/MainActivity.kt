18cf6ed1b623cebfa7b05f87d6e1e81ef6ce46e5
Yaxi Ye <yaxi.ye@gmail.com>
2024-06-29 17:39:26 +0100

    separate out main screen from activity

--- app/src/main/java/com/yeyaxi/creditcardvalidation/presentation/MainActivity.kt
+++ app/src/main/java/com/yeyaxi/creditcardvalidation/presentation/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.yeyaxi.creditcardvalidation.presentation.ui.screen.MainScreen
 import com.yeyaxi.creditcardvalidation.presentation.ui.theme.CreditCardValidationTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,29 +16,8 @@ class MainActivity: ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CreditCardValidationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CreditCardValidationTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/yeyaxi/creditcardvalidation/presentation/ui/screen/MainScreen.kt
@@ -0,0 +1,36 @@
+package com.yeyaxi.creditcardvalidation.presentation.ui.screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.yeyaxi.creditcardvalidation.presentation.ui.theme.CreditCardValidationTheme
+
+@Composable
+fun MainScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CreditCardValidationTheme {
+        Greeting("Android")
+    }
+}
