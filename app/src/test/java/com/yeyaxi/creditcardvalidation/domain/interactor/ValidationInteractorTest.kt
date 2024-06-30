16ca7712e6a9b4d4cfd21a6c941c4b0cc1fa9031
Yaxi Ye <yaxi.ye@gmail.com>
2024-06-30 19:06:51 +0100

    update unit test accordingly

---
+++ app/src/test/java/com/yeyaxi/creditcardvalidation/domain/interactor/ValidationInteractorTest.kt
@@ -0,0 +1,144 @@
+package com.yeyaxi.creditcardvalidation.domain.interactor
+
+import com.yeyaxi.creditcardvalidation.data.CardType
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertNull
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class ValidationInteractorTest {
+
+    private lateinit var interactor: ValidationInteractor
+
+    @Before
+    fun setup() {
+        interactor = ValidationInteractor()
+    }
+
+    @Test
+    fun `test validate with empty number expect error is null`() {
+        runTest {
+            val result = interactor.invoke("")
+            assertTrue(result.isSuccess)
+            assertEquals(CardType.NONE, result.getOrNull())
+        }
+    }
+
+    @Test
+    fun `test validate with valid amex number expect cardType is AMEX`() {
+        runTest {
+            Data.DUMMY_AMEX.forEach {
+                val result = interactor.invoke(it)
+                assertTrue(result.isSuccess)
+                assertEquals(CardType.AMEX, result.getOrNull())
+            }
+        }
+    }
+
+    @Test
+    fun `test validate with valid discover number expect cardType is Discover`() {
+        runTest {
+            val result = interactor.invoke(Data.DUMMY_DISCOVER)
+            assertTrue(result.isSuccess)
+            assertEquals(CardType.DISCOVER, result.getOrNull())
+        }
+    }
+
+    @Test
+    fun `test validate with valid mastercard number expect cardType is Mastercard`() {
+        runTest {
+            Data.DUMMY_MASTERCARD.forEach {
+                val result = interactor.invoke(it)
+                assertTrue(result.isSuccess)
+                assertEquals(CardType.MASTERCARD, result.getOrNull())
+            }
+        }
+    }
+
+    @Test
+    fun `test validate with valid visa number expect cardType is Visa`() {
+        runTest {
+            Data.DUMMY_VISA.forEach {
+                val result = interactor.invoke(it)
+                assertTrue(result.isSuccess)
+                assertEquals(CardType.VISA, result.getOrNull())
+            }
+        }
+    }
+
+    @Test
+    fun `test validate with invalid numbers expect error with Invalid Number`() {
+        runTest {
+            Data.INVALID_NUMBERS.forEach {
+                val result = interactor.invoke(it)
+                assertFalse(result.isSuccess)
+                assertTrue(result.isFailure)
+                val t = result.exceptionOrNull()
+                assertEquals("Invalid Number", t?.message)
+                assertNull(result.getOrNull())
+            }
+        }
+    }
+
+    @Test
+    fun `test validate with valid number and not from Amex Discover MasterCard or Visa expect cardType is UNKNOWN`() {
+        runTest {
+            Data.VALID_UNKNOWN_NUMBERS.forEach {
+                val result = interactor.invoke(it)
+                assertTrue(result.isSuccess)
+                assertEquals(CardType.UNKNOWN, result.getOrNull())
+            }
+        }
+    }
+
+    object Data {
+
+        val DUMMY_AMEX = arrayOf(
+            "345678901234564",
+            "378282246310005"
+        )
+        const val DUMMY_DISCOVER = "6011111111111117"
+        val DUMMY_MASTERCARD = arrayOf(
+            "5105105105105100",
+            "5200007840000022",
+            "5301745529138831",
+            "5404000000000001",
+            "5555558265554449"
+        )
+        val DUMMY_VISA = arrayOf(
+            "4007000000027",
+            "4111111111111111"
+        )
+        val INVALID_NUMBERS = arrayOf(
+            "34567890123451",
+            "37",
+            "37828224631000",
+            "398282246310005",
+            "6011",
+            "601111111111",
+            "5005105105105100",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "5105105105105107",
+            "5200007840000021",
+            "5301745529138832",
+            "5404000000000002",
+            "5555558265554440",
+            "4007000000028",
+            "4",
+            "4711111111111111",
+        )
+        val VALID_UNKNOWN_NUMBERS = arrayOf(
+            "34", // incomplete
+            "3056930009020004", // Diners Club
+            "3530111333300000", // JCB
+            "6200000000000005" // UnionPay
+        )
+    }
+}
--- app/src/test/java/com/yeyaxi/creditcardvalidation/presentation/viewmodel/MainViewModelTest.kt
+++ app/src/test/java/com/yeyaxi/creditcardvalidation/presentation/viewmodel/MainViewModelTest.kt
@@ -2,8 +2,12 @@ package com.yeyaxi.creditcardvalidation.presentation.viewmodel
 
 import CoroutineTestRule
 import app.cash.turbine.test
-import com.yeyaxi.creditcardvalidation.domain.CardType
-import com.yeyaxi.creditcardvalidation.domain.CoroutineDispatchers
+import com.yeyaxi.creditcardvalidation.data.CardType
+import com.yeyaxi.creditcardvalidation.domain.interactor.ValidationInteractor
+import com.yeyaxi.creditcardvalidation.domain.util.CoroutineDispatchers
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNull
 import kotlinx.coroutines.test.runTest
@@ -24,10 +28,15 @@ class MainViewModelTest {
             main = coroutineTestRule.dispatcher
         )
     }
+    private lateinit var interactor: ValidationInteractor
 
     @Before
     fun setup() {
-        mainViewModel = MainViewModel(dispatchers)
+        interactor = mockk()
+        mainViewModel = MainViewModel(
+            dispatchers = dispatchers,
+            validationInteractor = interactor
+        )
     }
 
     @Test
@@ -46,26 +55,26 @@ class MainViewModelTest {
     }
 
     @Test
-    fun `test validate with valid amex number expect cardType is AMEX`() {
-        Data.DUMMY_AMEX.forEach {
-            mainViewModel.validate(it)
-            runTest {
-                mainViewModel.viewState.test {
-                    val item = awaitItem()
-                    val cardType = item.cardType
-                    val error = item.error
-                    assertNull(error)
-                    assertEquals(CardType.AMEX, cardType)
-                    cancelAndIgnoreRemainingEvents()
-                }
+    fun `test validate when validator returns success expect error is null and cardType is set`() {
+        runTest {
+            coEvery { interactor.invoke(any()) } returns Result.success(CardType.AMEX)
+            mainViewModel.validate(Data.DUMMY_AMEX)
+            mainViewModel.viewState.test {
+                val item = awaitItem()
+                val cardType = item.cardType
+                val error = item.error
+                assertNull(error)
+                assertEquals(CardType.AMEX, cardType)
+                cancelAndIgnoreRemainingEvents()
             }
         }
     }
 
     @Test
     fun `test validate with valid discover number expect cardType is Discover`() {
-        mainViewModel.validate(Data.DUMMY_DISCOVER)
         runTest {
+            coEvery { interactor.invoke(any()) } returns Result.success(CardType.DISCOVER)
+            mainViewModel.validate(Data.DUMMY_DISCOVER)
             mainViewModel.viewState.test {
                 val item = awaitItem()
                 val cardType = item.cardType
@@ -79,118 +88,76 @@ class MainViewModelTest {
 
     @Test
     fun `test validate with valid mastercard number expect cardType is Mastercard`() {
-        Data.DUMMY_MASTERCARD.forEach {
-            mainViewModel.validate(it)
-            runTest {
-                mainViewModel.viewState.test {
-                    val item = awaitItem()
-                    val cardType = item.cardType
-                    val error = item.error
-                    assertNull(error)
-                    assertEquals(CardType.MASTERCARD, cardType)
-                    cancelAndIgnoreRemainingEvents()
-                }
+        runTest {
+            coEvery { interactor.invoke(any()) } returns Result.success(CardType.MASTERCARD)
+            mainViewModel.validate(Data.DUMMY_MASTERCARD)
+            mainViewModel.viewState.test {
+                val item = awaitItem()
+                val cardType = item.cardType
+                val error = item.error
+                assertNull(error)
+                assertEquals(CardType.MASTERCARD, cardType)
+                cancelAndIgnoreRemainingEvents()
             }
         }
+
     }
 
     @Test
     fun `test validate with valid visa number expect cardType is Visa`() {
-        Data.DUMMY_VISA.forEach {
-            mainViewModel.validate(it)
-            runTest {
-                mainViewModel.viewState.test {
-                    val item = awaitItem()
-                    val cardType = item.cardType
-                    val error = item.error
-                    assertNull(error)
-                    assertEquals(CardType.VISA, cardType)
-                    cancelAndIgnoreRemainingEvents()
-                }
+        runTest {
+            coEvery { interactor.invoke(any()) } returns Result.success(CardType.VISA)
+            mainViewModel.validate(Data.DUMMY_VISA)
+            mainViewModel.viewState.test {
+                val item = awaitItem()
+                val cardType = item.cardType
+                val error = item.error
+                assertNull(error)
+                assertEquals(CardType.VISA, cardType)
+                cancelAndIgnoreRemainingEvents()
             }
         }
+
     }
 
     @Test
     fun `test validate with invalid numbers expect error with Invalid Number cardType is None`() {
-        Data.INVALID_NUMBERS.forEach {
-            mainViewModel.validate(it)
-            println(it)
-            runTest {
-                mainViewModel.viewState.test {
-                    val item = awaitItem()
-                    val num = item.cardNum
-                    val cardType = item.cardType
-                    val error = item.error
-                    assertEquals("Invalid Number", error?.message)
-                    assertEquals(CardType.NONE, cardType)
-                    cancelAndIgnoreRemainingEvents()
-                }
+        runTest {
+            coEvery { interactor.invoke(any()) } returns Result.failure(Throwable("Invalid Number"))
+            mainViewModel.validate("1234")
+            mainViewModel.viewState.test {
+                val item = awaitItem()
+                val cardType = item.cardType
+                val error = item.error
+                assertEquals("Invalid Number", error?.message)
+                assertEquals(CardType.NONE, cardType)
+                cancelAndIgnoreRemainingEvents()
             }
         }
     }
 
     @Test
     fun `test validate with valid number and not from Amex Discover MasterCard or Visa expect cardType is UNKNOWN`() {
-        Data.VALID_UNKNOWN_NUMBERS.forEach {
-            mainViewModel.validate(it)
-            runTest {
-                mainViewModel.viewState.test {
-                    val item = awaitItem()
-                    val cardType = item.cardType
-                    val error = item.error
-                    assertNull(error)
-                    assertEquals(CardType.UNKNOWN, cardType)
-                    cancelAndIgnoreRemainingEvents()
-                }
+        runTest {
+            coEvery { interactor.invoke(any()) } returns Result.success(CardType.UNKNOWN)
+            mainViewModel.validate(Data.VALID_UNKNOWN_NUMBERS)
+            mainViewModel.viewState.test {
+                val item = awaitItem()
+                val cardType = item.cardType
+                val error = item.error
+                assertNull(error)
+                assertEquals(CardType.UNKNOWN, cardType)
+                cancelAndIgnoreRemainingEvents()
             }
         }
     }
 
     object Data {
-        val DUMMY_AMEX = arrayOf(
-            "345678901234564",
-            "378282246310005"
-        )
+
+        const val DUMMY_AMEX = "345678901234564"
         const val DUMMY_DISCOVER = "6011111111111117"
-        val DUMMY_MASTERCARD = arrayOf(
-            "5105105105105100",
-            "5200007840000022",
-            "5301745529138831",
-            "5404000000000001",
-            "5555558265554449"
-        )
-        val DUMMY_VISA = arrayOf(
-            "4007000000027",
-            "4111111111111111"
-        )
-        val INVALID_NUMBERS = arrayOf(
-            "34567890123451",
-            "37",
-            "37828224631000",
-            "398282246310005",
-            "6011",
-            "601111111111",
-            "5005105105105100",
-            "51",
-            "52",
-            "53",
-            "54",
-            "55",
-            "5105105105105107",
-            "5200007840000021",
-            "5301745529138832",
-            "5404000000000002",
-            "5555558265554440",
-            "4007000000028",
-            "4",
-            "4711111111111111",
-        )
-        val VALID_UNKNOWN_NUMBERS = arrayOf(
-            "34", // incomplete
-            "3056930009020004", // Diners Club
-            "3530111333300000", // JCB
-            "6200000000000005" // UnionPay
-        )
+        const val DUMMY_MASTERCARD = "5105105105105100"
+        const val DUMMY_VISA = "4007000000027"
+        const val VALID_UNKNOWN_NUMBERS = "3056930009020004"
     }
 }
